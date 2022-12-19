b16d6e4c593d979fbe32e5d038cf598e81cf6187
danshui-git <68673669+danshui-git@users.noreply.github.com>
2022-12-19 16:47:34 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -132,7 +132,7 @@ if [[ -n "$TELEGRAM_BOT_TOKEN" ]]; then
   Web：${WEB_LINE}" "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage"
   echo ""
 elif [[ -n "$PUSH_PLUS_TOKEN" ]]; then
-  curl -k --data token=${PUSH_PLUS_TOKEN} --data title=SSH_Message --data "content=SSH：${SSH_LINE}
+  curl -k --data token=${PUSH_PLUS_TOKEN} --data title="SSH连接代码" --data "content=SSH：${SSH_LINE}
   
   Web：${WEB_LINE}" "http://www.pushplus.plus/send"
   echo ""
