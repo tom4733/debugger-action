8cfcf368a5c5e87de63609b5377e3aad701e7f81
danshui-git <68673669+danshui-git@users.noreply.github.com>
2023-04-01 02:37:55 +0800

    Update tmate.sh

--- tmate.sh
+++ tmate.sh
@@ -32,7 +32,7 @@ else
     exit 1
 fi
 echo -e "${INFO} Check the version of tmate ..."
-curl -fsSL https://github.com/281677160/common-main/releases/download/API/tmate.api -o tmateapi
+curl -H "Authorization: Bearer ${REPO_TOKEN}" https://api.github.com/repos/tmate-io/tmate/releases/latest -o tmateapi
 tmate_ver=$(grep -o '"tag_name": ".*"' tmateapi | head -n 1 | sed 's/"//g;s/v//g' | sed 's/tag_name: //g')
 [ -z $tmate_ver ] && {
     echo -e "${ERROR} Unable to check the version, network failure or API error."
